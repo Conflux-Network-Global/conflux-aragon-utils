a87e5d1bf32e3c8231e9ad85a850f98ce15a697f
Thegaram <th307q@gmail.com>
2021-03-24 11:57:46 +0800

    get start epoch number through contract query

--- src/repos.js
+++ src/repos.js
@@ -8,24 +8,26 @@ require('dotenv').config()
 const NEW_REPO = Web3EthAbi.encodeEventSignature('NewRepo(bytes32,string,address)');
 const NODE_URL = 'http://test.confluxrpc.org';
 
-const raw = fs.readFileSync('repo.abi');
-const abi = JSON.parse(raw);
+const Repo = JSON.parse(fs.readFileSync('Repo.abi'));
+const APMRegistry = JSON.parse(fs.readFileSync('APMRegistry.abi'));
 
 const conflux = new Conflux({ url: NODE_URL });
 
-async function list(apm, epoch) {
-    epoch = parseInt(epoch, 10);
+async function list(apm) {
+    const fromEpoch = Number(await conflux.Contract({ abi: APMRegistry, address: apm }).getInitializationEpoch());
+    const toEpoch = Math.min(fromEpoch + 9999, await conflux.getEpochNumber('latest_state'))
+    console.log(`querying NewRepo logs between ${fromEpoch}..${toEpoch}...`);
 
     const repos = await conflux.getLogs({
         address: apm,
-        fromEpoch: epoch,
-        toEpoch: epoch + 9999,
+        fromEpoch,
+        toEpoch,
         topics: [[NEW_REPO]],
     });
 
     for (const raw of repos) {
         const { '1': repoName, '2': address } = Web3EthAbi.decodeParameters(['bytes32', 'string', 'address'], raw.data);
-        const latest = await conflux.Contract({ abi, address }).getLatest();
+        const latest = await conflux.Contract({ abi: Repo, address }).getLatest();
         console.log(`${repoName.padStart(20)} is at ${address} (latest version: ${latest.semanticVersion.join('.')} at ${latest.contractAddress}, contentURI: ${latest.contentURI.toString('utf8')})`);
     }
 }
@@ -34,7 +36,7 @@ async function register(repo, newVersion, newAddress, newContentURI) {
     newVersion = newVersion.split('.');
 
     const account = conflux.wallet.addPrivateKey(process.env.PRIVATE_KEY);
-    const contract = await conflux.Contract({ abi, address: repo });
+    const contract = await conflux.Contract({ abi: Repo, address: repo });
 
     const old = await contract.getLatest();
     console.log(`current version: ${old.semanticVersion.join('.')} at ${old.contractAddress}, contentURI: ${old.contentURI.toString('utf8')}`);
@@ -59,7 +61,7 @@ async function main() {
     const program = new Command();
 
     program
-        .command('list <apm> <epoch>')
+        .command('list <apm>')
         .description('clone a repository into a newly created directory')
         .action(list);
 
